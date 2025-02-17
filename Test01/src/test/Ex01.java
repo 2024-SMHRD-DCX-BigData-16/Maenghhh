6c5b173fe3d800dbbaaf206f2d943c15a8ec13f9
unknown <audgns1640@gmail.com>
2025-02-17 12:24:32 +0900

    두번째커밋

--- Test01/src/test/Ex01.java
+++ Test01/src/test/Ex01.java
@@ -3,8 +3,11 @@ package test;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Test01");
+		System.out.println("Test02");
+		
+		
+		
 	}
 
 }
